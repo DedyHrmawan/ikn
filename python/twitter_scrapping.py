6fb394563127839ab532d2af874e57ad44821b23
Miftahul Lubab <miftahul.lubab20@gmail.com>
2024-01-19 16:33:45 +0700

    :recycle: refactor: remove credentials on twitter_scrapping.py

--- python/twitter_scrapping.py
+++ python/twitter_scrapping.py
@@ -1,11 +1,12 @@
 import tweepy
+from dotenv import dotenv_values
 
-auth = tweepy.OAuth2BearerHandler(
-    "AAAAAAAAAAAAAAAAAAAAADbyrwEAAAAAUGr5fuaWzxa5Xb9s1U2NmxoevIw%3DkuEcLtGPSsdQh8Iaw8p7VdCirN2iuUFds2Cy2OROOu0WlsN6hL"
-)
+config = dotenv_values(".env")
+
+auth = tweepy.OAuth2BearerHandler(config.get("TWITTER_ACCESS_TOKEN"))
 api = tweepy.API(auth)
 
-KEYWORDS = '"ibu kota negara" (ikn OR ibu kota negara)'
+KEYWORDS = config.get("TWITTER_SEARCH_KEYWORD")
 
 # Basic keyword search
 tweets = api.search_tweets(KEYWORDS, result_type="recent", tweet_mode="extended")
